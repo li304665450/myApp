fc69e31a809c41e0f7d4e144609857f46e26876e
Mars-company <15600017090@qq.com>
2017-12-13 10:32:13 +0800

    文件目录调整

--- conf/api/config.php
+++ conf/api/config.php
@@ -14,7 +14,11 @@ return [
     // | api分组设置
     // +----------------------------------------------------------------------
 
+    // 应用调试模式
+    'app_debug'              => true,
     // 默认输出类型
     'default_return_type'    => 'json',
+    // 异常处理handle类 留空使用 \think\exception\Handle
+    'exception_handle'       => 'app\common\lib\exception\ApiHandleException',
 
 ];
--- conf/extra/app.php
+++ conf/extra/app.php
@@ -7,4 +7,13 @@
  */
 return [
     'password_pre_halt'  => '_#string_try',//密码加密盐
+    'aeskey' => 'ssg445566appccaa',//aes密钥
+    'app_types' => [//服务端设备类型
+        'ios',
+        'android'
+    ],
+    'app_sign_time' => 1000000,//sign授权码过期时间
+    'app_sign_cache_time' => 20,//sign授权码缓存失效时间
+    'login_timeout_days' => 7,
+    'default_username_prefix' => 'yiplay-粉'
 ];
